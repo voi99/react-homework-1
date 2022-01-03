557e4da45d151c50c3ae942afa5c284cf12acea6
Milos Danilovic <danilovicmilos25@gmail.com>
2022-01-03 20:16:12 +0100

    Made the form and added some validations

--- src/Task-1/FirstTask.js
+++ src/Task-1/FirstTask.js
@@ -1,11 +1,146 @@
-import React from 'react'
+// import React from "react";
 
-const FirstTask = () => {
-   return (
-      <div>
-         <p>First Task</p>
-      </div>
-   )
+// const FirstTask = () => {
+//   return (
+//     <div>
+//       <p>First Task</p>
+//     </div>
+//   );
+// };
+
+// export default FirstTask;
+import React from "react";
+import "./firstTask.css";
+
+const initialState = {
+  userName: "",
+  firstName: "",
+  lastName: "",
+  name: "",
+  email: "",
+  password: "",
+  confirmPassword: "",
+  nameError: "",
+  emailError: "",
+  passwordError: "",
+};
+
+export default class ValiationForm extends React.Component {
+  state = initialState;
+
+  handleChange = (event) => {
+    const isCheckbox = event.target.type === "checkbox";
+    this.setState({
+      [event.target.name]: isCheckbox
+        ? event.target.checked
+        : event.target.value,
+    });
+  };
+
+  validate = () => {
+    let nameError = "";
+    let emailError = "";
+    // let passwordError = "";
+
+    if (!this.state.lastName) {
+      nameError = "name cannot be blank";
+    }
+
+    if (!this.state.email.includes("@")) {
+      emailError = "invalid email";
+    }
+
+    if (emailError || nameError) {
+      this.setState({ emailError, nameError });
+      return false;
+    }
+
+    return true;
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const isValid = this.validate();
+    if (isValid) {
+      console.log(this.state);
+      // clear form
+      this.setState(initialState);
+    }
+  };
+
+  render() {
+    return (
+      <form className="firstTaskForm" onSubmit={this.handleSubmit}>
+        <div>
+          <input
+            name="userName"
+            placeholder="UserName"
+            value={this.state.userName}
+            onChange={this.handleChange}
+          />
+          <div style={{ fontSize: 12, color: "red" }}>
+            {this.state.nameError}
+          </div>
+        </div>
+        <div>
+          <input
+            name="firstName"
+            placeholder="FirstName"
+            value={this.state.firstName}
+            onChange={this.handleChange}
+          />
+          <div style={{ fontSize: 12, color: "red" }}>
+            {this.state.nameError}
+          </div>
+        </div>
+        <div>
+          <input
+            name="lastName"
+            placeholder="LastName"
+            value={this.state.lastName}
+            onChange={this.handleChange}
+          />
+          <div style={{ fontSize: 12, color: "red" }}>
+            {this.state.nameError}
+          </div>
+        </div>
+        <div>
+          <input
+            name="email"
+            placeholder="Email"
+            value={this.state.email}
+            onChange={this.handleChange}
+          />
+          <div style={{ fontSize: 12, color: "red" }}>
+            {this.state.emailError}
+          </div>
+        </div>
+        <div>
+          <input
+            type="password"
+            name="password"
+            placeholder="password"
+            value={this.state.password}
+            onChange={this.handleChange}
+          />
+          <div style={{ fontSize: 12, color: "red" }}>
+            {this.state.passwordError}
+          </div>
+        </div>
+        <div>
+          <input
+            type="password"
+            name="confirmPassword"
+            placeholder="confirmPassword"
+            value={this.state.confirmPassword}
+            onChange={this.handleChange}
+          />
+          <div style={{ fontSize: 12, color: "red" }}>
+            {this.state.passwordError}
+          </div>
+        </div>
+        <button type="submit">submit</button>
+      </form>
+    );
+  }
 }
-
-export default FirstTask
