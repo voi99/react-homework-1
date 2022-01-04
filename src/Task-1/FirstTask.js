bdb4e283aee2bbf2c0bd4b033ed47a5a33fb3620
Milos Danilovic <danilovicmilos25@gmail.com>
2022-01-04 19:52:33 +0100

    Finished the first Task

--- src/Task-1/FirstTask.js
+++ src/Task-1/FirstTask.js
@@ -1,19 +1,31 @@
 import React from "react";
 import "./firstTask.css";
-
+import InputField from "./components/formInputField";
+import {
+  NotificationContainer,
+  NotificationManager,
+} from "react-notifications";
+import "react-notifications/lib/notifications.css";
 const initialState = {
-  confirmPassword: "",
   userName: "",
   firstName: "",
   lastName: "",
+  email: "",
   password: "",
+  confirmPassword: "",
+  userNameError: "",
   emailError: "",
   passwordError: "",
-  userNameError: "",
-  email: "",
+  confirmPasswordError: "",
+  userNameErrorTrue: "",
+  emailErrorTrue: "",
+  passwordErrorTrue: "",
+  firstNameErrorTrue: "",
+  lastNameErrorTrue: "",
+  confirmPasswordErrorTrue: "",
 };
 
-export default class ValiationForm extends React.Component {
+export default class ValidationForm extends React.Component {
   state = initialState;
 
   handleChange = (event) => {
@@ -35,30 +47,46 @@ export default class ValiationForm extends React.Component {
     let emailregex = /^[^@\s]+@[^@\s]+\.[^@\s]+$/;
     let passwordRegEx =
       /(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~])[A-Za-z\d!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]{8,}$/;
+
+    let userNameErrorTrue = "";
+    let firstNameErrorTrue = "";
+    let lastNameErrorTrue = "";
+    let emailErrorTrue = "";
+    let passwordErrorTrue = "";
+    let confirmPasswordErrorTrue = "";
     if (!this.state.lastName) {
       lastNameError = "Last name cannot be blank";
+      lastNameErrorTrue = "inputError";
     }
     if (!this.state.firstName) {
       firstNameError = "First name cannot be blank";
+      firstNameErrorTrue = "inputError";
     }
 
     if (!this.state.userName) {
       userNameError = "username cannot be blank";
+      userNameErrorTrue = "inputError";
     } else if (6 > this.state.userName.length) {
       userNameError = "username is too Short";
+      userNameErrorTrue = "inputError";
     } else if (this.state.userName.length > 12) {
       userNameError = "username is too Long";
+      userNameErrorTrue = "inputError";
     }
 
     if (!emailregex.test(this.state.email)) {
-      emailError = "invalid email";
-      // ^[^@\s]+@[^@\s]+\.[^@\s]+$
+      emailError = "Invalid email";
+
+      emailErrorTrue = "inputError";
     }
     if (!passwordRegEx.test(this.state.password)) {
-      passwordError = "Password is too short";
+      passwordError =
+        "Password must have at least 1 capital letter,number and a special character";
+      passwordErrorTrue = "inputError";
     }
     if (this.state.password !== this.state.confirmPassword) {
       confirmPasswordError = "Password does not match";
+      confirmPasswordErrorTrue = "inputError";
     }
     if (
       emailError ||
@@ -70,12 +98,17 @@ export default class ValiationForm extends React.Component {
     ) {
       this.setState({
         emailError,
-
         userNameError,
         lastNameError,
         passwordError,
         firstNameError,
         confirmPasswordError,
+        userNameErrorTrue,
+        emailErrorTrue,
+        firstNameErrorTrue,
+        lastNameErrorTrue,
+        passwordErrorTrue,
+        confirmPasswordErrorTrue,
       });
       return false;
     }
@@ -87,7 +120,6 @@ export default class ValiationForm extends React.Component {
     event.preventDefault();
     const isValid = this.validate();
     if (isValid) {
-      console.log(this.state);
       const formFields = {
         userName: this.state.userName,
         firstName: this.state.firstName,
@@ -107,87 +139,95 @@ export default class ValiationForm extends React.Component {
       })
         .then(function (response) {
           let blobUrl = response.headers.get("Location");
+
+          NotificationManager.success(
+            "Congratulations you have successfully registered! ",
+            "Success!"
+          );
           console.log(blobUrl, "BLOB URL");
         })
         .catch(function (error) {
           console.log(error);
+          NotificationManager.error(
+            "Something went wrong please try again! ",
+            "Error!"
+          );
         });
     }
   };
 
   render() {
     return (
-      <form className="firstTaskForm" onSubmit={this.handleSubmit}>
-        <div>
-          <input
+      <div className="firstTaskContainer">
+        <NotificationContainer />
+        <div className="firstTaskImageContainer">
+          <img src="/images/registerImage.svg" alt="" />
+        </div>
+        <form className="firstTaskForm" onSubmit={this.handleSubmit}>
+          <div className="firstTaskRegisterText">REGISTER WITH US</div>
+          <InputField
+            type="text"
             name="userName"
-            placeholder="UserName"
+            placeholder="Username"
+            icon="fa fa-user icon"
             value={this.state.userName}
-            onChange={this.handleChange}
+            class={this.state.userNameErrorTrue}
+            errorMessage={this.state.userNameError}
+            handleChange={this.handleChange}
           />
-          <div style={{ fontSize: 12, color: "red" }}>
-            {this.state.userNameError}
-          </div>
-        </div>
-        <div>
-          <input
+          <InputField
+            type="text"
             name="firstName"
-            placeholder="FirstName"
+            placeholder="First Name"
+            icon="fas fa-signature"
             value={this.state.firstName}
-            onChange={this.handleChange}
+            class={this.state.firstNameErrorTrue}
+            errorMessage={this.state.firstNameError}
+            handleChange={this.handleChange}
           />
-          <div style={{ fontSize: 12, color: "red" }}>
-            {this.state.firstNameError}
-          </div>
-        </div>
-        <div>
-          <input
+          <InputField
+            type="text"
             name="lastName"
-            placeholder="LastName"
+            placeholder="Last Name"
+            icon="fas fa-signature"
             value={this.state.lastName}
-            onChange={this.handleChange}
+            class={this.state.lastNameErrorTrue}
+            errorMessage={this.state.lastNameError}
+            handleChange={this.handleChange}
           />
-          <div style={{ fontSize: 12, color: "red" }}>
-            {this.state.lastNameError}
-          </div>
-        </div>
-        <div>
-          <input
+          <InputField
+            type="text"
             name="email"
             placeholder="Email"
+            icon="fa fa-envelope icon"
             value={this.state.email}
-            onChange={this.handleChange}
+            class={this.state.emailErrorTrue}
+            errorMessage={this.state.emailError}
+            handleChange={this.handleChange}
           />
-          <div style={{ fontSize: 12, color: "red" }}>
-            {this.state.emailError}
-          </div>
-        </div>
-        <div>
-          <input
+          <InputField
             type="password"
             name="password"
-            placeholder="password"
+            placeholder="Password"
+            icon="fa fa-key icon"
             value={this.state.password}
-            onChange={this.handleChange}
+            class={this.state.passwordErrorTrue}
+            errorMessage={this.state.passwordError}
+            handleChange={this.handleChange}
           />
-          <div style={{ fontSize: 12, color: "red" }}>
-            {this.state.passwordError}
-          </div>
-        </div>
-        <div>
-          <input
+          <InputField
             type="password"
             name="confirmPassword"
-            placeholder="confirmPassword"
+            placeholder="Confirm Password"
+            icon="fas fa-unlock-alt"
             value={this.state.confirmPassword}
-            onChange={this.handleChange}
+            class={this.state.confirmPasswordErrorTrue}
+            errorMessage={this.state.confirmPasswordError}
+            handleChange={this.handleChange}
           />
-          <div style={{ fontSize: 12, color: "red" }}>
-            {this.state.confirmPasswordError}
-          </div>
-        </div>
-        <button type="submit">submit</button>
-      </form>
+          <button type="submit">SUBMIT</button>
+        </form>
+      </div>
     );
   }
 }
