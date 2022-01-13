6d11dc00c033ce7b475b003c47105b7724384b92
Ivan Popovic <ivanivopop@gmail.com>
2022-01-13 18:49:55 +0100

    single post page done

--- src/Task-3/components/Post.js
+++ src/Task-3/components/Post.js
@@ -1,21 +1,46 @@
 import React, { Component } from 'react'
 import withRouter from '../../withRouter'
+import styles from './Post.module.css'
+import LoadingSpinner from './LoadingSpinner'
 
 class Post extends Component {
    state = {
       post: {},
+      loading: true,
    }
 
-   componentDidMount() {
-      const post = this.props.posts.filter(
-         (post) => post.id === this.props.params.id
-      )[0]
+   async componentDidMount() {
+      try {
+         const response = await fetch(
+            'https://jsonblob.com/api/930078800407183360'
+         )
+         const posts = await response.json()
 
-      this.setState({ post: post })
+         const post = posts.filter(
+            (post) => post.id === this.props.params.id
+         )[0]
+
+         this.setState({ post: post, loading: false })
+      } catch (err) {
+         console.log(err)
+      }
    }
 
    render() {
-      return <div>{this.state.post.content}</div>
+      return this.state.loading ? (
+         <LoadingSpinner />
+      ) : (
+         <div className={styles.post}>
+            <div className={styles.banner}>
+               <img src={this.state.post.src} alt='post-img' />
+               <p className={styles.author}>{this.state.post.author}</p>
+            </div>
+            <div className={styles.text}>
+               <h3 className={styles.title}>{this.state.post.title}</h3>
+               <p>{this.state.post.content}</p>
+            </div>
+         </div>
+      )
    }
 }
 
